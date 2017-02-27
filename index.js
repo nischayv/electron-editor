8f6b58fe3f9121784c6715abd4cfaaa4815f0350
Nischay Venkatram <nischay.v@gmail.com>
2017-02-26 23:08:55 -0800

    Updated CSS

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const path = require('path')
 let mainWindow;
 
 app.on('ready', () => {
-  const index = path.join(__dirname, 'index.html');
-  // /Users/Nischay/../index.html
+    const index = path.join(__dirname, 'index.html');
+    // /Users/Nischay/../index.html
 
-  mainWindow = new BrowserWindow({});
-  mainWindow.loadURL(`file:///${index}`);
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file:///${index}`);
 
-  mainWindow.webContents.on('will-navigate', (e, url) => {
-    e.preventDefault();
-    console.log(`Tried to navigate to ${url}`);
+    mainWindow.webContents.on('will-navigate', (e, url) => {
+        e.preventDefault();
+        console.log(`Tried to navigate to ${url}`);
 
-    mainWindow.webContents.send('open-file', url);
-  });
+        mainWindow.webContents.send('open-file', url);
+    });
 });
